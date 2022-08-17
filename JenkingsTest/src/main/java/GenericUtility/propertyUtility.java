54ca8de99244f7ea33af297a4c29fad3dca9b599
ChetanMahale948 <chetan.mahale948@gmail.com>
2022-08-17 18:10:53 +0530

    i have committed

---
+++ JenkingsTest/src/main/java/GenericUtility/propertyUtility.java
@@ -0,0 +1,18 @@
+package GenericUtility;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+public class propertyUtility
+{
+   public String getPropertyKeyValue(String Key) throws Throwable 
+   {
+	FileInputStream fis = new FileInputStream("./Data/data.property");
+	Properties pro=new Properties();
+	pro.load(fis);
+	String value = pro.getProperty(Key);
+	return value;
+}
+}
