b2894cb1eab71c2add1e284fee32078f205118bf
ChetanMahale948 <chetan.mahale948@gmail.com>
2022-08-18 15:39:35 +0530

    i am commited

---
+++ Example/src/test/java/JenkinsExamplePractise/DemoTest.java
@@ -0,0 +1,37 @@
+package JenkinsExamplePractise;
+
+import java.io.FileInputStream;
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class DemoTest {
+	@Test
+	public void demoTest() throws Throwable {
+		
+		WebDriver driver=new ChromeDriver();
+		
+		FileInputStream fis = new FileInputStream("./data.property");
+		Properties pro=new Properties();
+		pro.load(fis);
+		System.out.println("Login To Application");
+		String Url=pro.getProperty("url");
+		//String UN=pro.getProperty("un");
+		//String PWD=pro.getProperty("pwd");
+		
+		driver.get(Url);
+		
+		System.out.println("Url");
+		System.out.println("Welcome to jenkins");
+		System.out.println("Hi jenkins");
+		
+		
+		
+		driver.quit();
+			
+		
+	}
+
+}
